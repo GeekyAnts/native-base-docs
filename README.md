170148c1d6c428dd8de74285b24a82f13aae9118
Supriya Kalghatgi <supriya@Supriyas-Air.blr-office.geekyants.com>
2018-10-16 09:23:30 +0530

    added script

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-4139297826447529",
+    enable_page_level_ads: true
+  });
+</script>
+
 {% include "/docs/introduction/README.md" %}
 
--- _book/gitbook/gitbook-plugin-codetabs/codetabs.js
+++ _book/gitbook/gitbook-plugin-codetabs/codetabs.js
@@ -1,47 +1,12 @@
-require(["jquery"], function($) {
-  var setupPage = function() {
-    if (localStorage) {
-      var lastSelected = localStorage.getItem("codetabs-selected");
-      if (lastSelected) {
-        $(".codetabs").each(function() {
-          var $tabs = $(this);
-          var tabId = $tabs
-            .find(".codetabs-header .tab")
-            .filter(function() {
-              return $(this).text() == lastSelected;
-            })
-            .data("codetab");
-          if (typeof tabId !== "undefined") {
-            setActiveTab($tabs, tabId);
-          }
-        });
-      }
-    }
-  };
+require([
+    'jquery'
+], function($) {
+    $(document).on('click.plugin.codetabs', '.codetabs .codetabs-header .tab', function(e) {
+        var $btn = $(e.target);
+        var $tabs = $btn.parents('.codetabs');
+        var tabId = $btn.data('codetab');
 
-  var setActiveTab = function($tabs, tabId) {
-    // console.log("$tabs: ", $tabs);
-    // console.log("tabId: ", tabId);
-    // console.log("find: ", $tabs.find(".tab"));
-
-    $tabs.find(".tab").removeClass("active");
-    $tabs.find('.tab[data-codetab="' + tabId + '"]').addClass("active");
-  };
-
-  $(document).on(
-    "click.plugin.codetabs",
-    ".codetabs .codetabs-header .tab",
-    function(e) {
-      var $btn = $(e.target);
-      var $tabs = $btn.parents(".codetabs");
-      var tabId = $btn.data("codetab");
-      setActiveTab($tabs, tabId);
-      if (localStorage) {
-        localStorage.setItem("codetabs-selected", $btn.text());
-      }
-    }
-  );
-
-  gitbook.events.bind("start", setupPage);
-  gitbook.events.bind("page.change", setupPage);
+        $tabs.find('.tab').removeClass('active');
+        $tabs.find('.tab[data-codetab="' + tabId + '"]').addClass('active');
+    });
 });
